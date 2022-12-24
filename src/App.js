08803cb9674aa873af698e51c8203cc5474d7425
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-12-24 10:42:13 -0500

    cart context, dropdown, icon

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route element={<Navigation />} path="/">
         <Route index element={<Home />} />
+        <Route element={<Shop />} path="shop" />
         <Route element={<Authentication />} path="auth" />
       </Route>
     </Routes>
